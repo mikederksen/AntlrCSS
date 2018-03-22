54d4065ee12a8caa8eaea0a399b30bce94fe9ff6
Mike Derksen <m.derksen@avisi.nl>
2018-03-22 16:33:46 +0100

    Reset generator

--- src/main/java/nl/han/ica/icss/generator/Generator.java
+++ src/main/java/nl/han/ica/icss/generator/Generator.java
@@ -2,28 +2,10 @@ package nl.han.ica.icss.generator;
 
 import nl.han.ica.icss.ast.*;
 import java.util.ArrayList;
-import java.util.List;
 
 public class Generator {
 
 	public String generate(AST ast) {
-        ASTNode root = ast.root;
-       	StringBuilder builder = new StringBuilder();
-
-        if(root != null) {
-			addToBuilder(root, builder, 0);
-
-			return builder.toString();
-		}
-
-        return "Empty...";
-	}
-
-	private void addToBuilder(ASTNode node, StringBuilder builder, int indent) {
-		builder.append(node.getNodeLabel());
-
-		for(ASTNode child : node.getChildren()) {
-		    addToBuilder(child, builder, indent + 1);
-        }
+        return "IMPLEMENT THIS";
 	}
 }
